d1528cbac1ff1cfef2939e0dde13f58a3d893e4e
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-02-07 15:59:01 +0530

    space.c

---
+++ space.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int i, sp;
+char a[30];
+printf("enter the string");
+scanf("%s", a);
+for(i=0 ; a[0]!="\0" ; i++
+{
+if (a[i]=' ')
+{
+spaces++;
+}
+}
+printf("Number of spaces is %d", sp);
+}
