0c9222adb5cc68919d6ffaf19cc2c0476d30517d
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-02-07 16:30:49 +0530

    fibonacci.c

---
+++ fibonnaci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+int v = 0, p = 1, i;
+if (n < 1)
+ return;
+for (i = 1; i <= n; i++)
+{
+printf("%d ", f2);
+int next = v + p;
+v = p;
+p = next;
+}
+}
+int main()
+{
+printFibonacciNumbers(7);
+return 0;
+}
