6fc48842157c4f2c50d0cb96d03871ca81a1d1c7
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-02-09 15:53:43 +0530

    Update strnprint.c

--- strnprint.c
+++ strnprint.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
-void main()
+void printing_char (char ch, int numberOfChars, int numberOfLines);
+int main (void)
 {
-char arr[100];
-printf("Enter a string\n");
-scanf("%s", arr);
-printf("You entered the string %s\n",arr);
-getch();
+char userChar;
+int lin,tim;
+printf ("please enter a character, number of times in a line, and number of lines:\n");
+while ((scanf ("%c%d%d", &userChar, &tim, &lin)) == 3)
+{
+printing_char (userChar, tim, lin);
+}
+return 0;
+}
+void printing_char (char ch, int numberOfChar, int numberOfLin)
+{
+int x;
+int y = 0;
+while (++y <= numberOfLin)
+{
+for (x = 0; x<numberOfChar; x++)
+{
+printf ("%c", ch);
+}
+printf ("\n");
+}
 }
