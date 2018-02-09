6f352dfc934c8218aa98a3bd45bec0a59f55a46b
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-02-09 16:15:46 +0530

    numdigit.c

---
+++ numdigit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+long long num;
+int cnt = 0;
+printf("Enter an integer: ");
+scanf("%lld", &num);
+while(n != 0)
+{
+n /= 10;
+++count;
+}
+printf("Number of digits: %d", cnt);
+}
