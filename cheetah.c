b6b622d63b63e81681ad712a0fba4974fa5b632c
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-02-12 16:18:40 +0530

    cheetah.c

---
+++ cheetah.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+#include<conio.h>
+void main()
+{
+int v,p;
+printf("\t Enter the Number:");
+scanf("%d",&v);
+p=(n&(n-1));
+if(p==0)
+printf("Yes",v);
+else
+printf("No",v);
+getch();
+ }
