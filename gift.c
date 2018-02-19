789cfb2ef409bf0b367424f6b781659ccefb091a
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-02-19 16:40:31 +0530

    gift.c

---
+++ gift.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+int v,p,m;
+Printf("Enter v,p values:");
+scanf("%d%d",&v,&p);
+m=v*p;
+printf("The Product of two num is:%d",m);
+if(m%2==0)
+{
+printf("The Product is Enen");
+}
+else
+{
+printf("The product is ODD");
+}
+getch();
+}
