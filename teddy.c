160fa4fc52241abb12586f939c179e837929020d
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-02-23 16:50:28 +0530

    teddy.c

---
+++ teddy.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char str[100];
+int num;
+printf("Please write something: \n");
+scanf("%s", str);
+printf("Enter number:");
+scanf("%d",&num);
+printf("%s %d",str,n);
+str=/0;
+printf("%s",str);
+getch();
+}
