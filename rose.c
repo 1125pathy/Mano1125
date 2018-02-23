3547962b64346e4c1d31291d8bb169b914373f63
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-02-23 16:27:36 +0530

    rose.c

---
+++ rose.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main() 
+{
+int v; 
+printf("enter a no."); 
+scanf("%d",&v); 
+if(v%10==0 || v%10==1) 
+printf("yes binary"); 
+else
+printf("not binary"); 
+getch(); 
+} 
+
