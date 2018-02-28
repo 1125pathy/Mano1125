e1b506112808b2307426a149c897098ae8b3190e
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-02-28 16:03:49 +0530

    udam.c

---
+++ udam.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main(int s,char *str[])
+{
+int l,a,count;
+char *str[]="Vinoth ravi";
+a=a_to_i(str[]);
+l=strlen(str);
+for(count=0;count<1;count++)
+{
+if(str[count]=='a'&&'A'||str[count]=='e'&&'E'||str[count]=='i'&&'I'||str[count]=='o'&&'O'||str[count]=='u'&&'U';
+{
+printf("vowel at position[%d}:%c\n",count,str[count]);
+}}
+return 0;
+}
