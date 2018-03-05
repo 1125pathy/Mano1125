0f54b8eb277d641e15a7e880c9f09524e7366c55
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-05 15:50:16 +0530

    picture.c

---
+++ picture.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+float n;
+printf("Enter the number:-");
+scanf("%f",&n);
+printf("The integer number after rounded is  %5.0f",n);
+getch();
+}
