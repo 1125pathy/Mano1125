597948803ba47c99693e0eb7ff81fd06252b8e1f
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-05 16:44:31 +0530

    own.c

---
+++ own.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void main()
+{
+int v,p,l;
+printf("Print some valid interval range:\n");
+scanf("%d%d",&v,&p);
+printf("print the number u want to check\n");
+scanf("%d",l);
+{
+if((p-1)==l)
+{
+printf("%d is in available in this interval",l);
+}
+if((p-2)==l)
+{
+printf("%d is in available in this interval",l);
+}
+if((p-3)==l)
+{
+printf("%d is in available in this interval",l);
+}
+if((p-4)==l)
+{
+printf("%d is in available in this interval",l);
+}
+if((p-5)==l)
+{
+printf("%d is in available in this interval",l);
+}
+if((p-6)==l)
+{
+printf("%d is in available in this interval",l);
+}
+if((p-7)==l)
+{
+printf("%d is in available in this interval",l);
+}
+if((p-8)==l)
+{
+printf("%d is in available in this interval",l);
+}
+if((p-9)==l)
+{
+printf("%d is in available in this interval",l);
+}
+}
+getch();
+}
