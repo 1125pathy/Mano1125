7c38c89a98b62dfbc5912df440f2426daf0e2fba
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-05 16:02:39 +0530

    fly.c

---
+++ fly.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int num;
+int v;
+float p;
+printf("Enter an integer number: ");
+scanf("%d",&num);
+fVar=sqrt((double)num);
+v=p;
+if(v==p)
+printf("%d is a perfect square.",num);
+else
+printf("%d is not a perfect square.",num);
+getch();
+}
