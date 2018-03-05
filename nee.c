e1ba62320b75c3f084645d5bd16f38a7e95cff15
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-05 16:54:47 +0530

    nee.c

---
+++ nee.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[15];
+int v,p;
+scanf("%d",s);
+v=strlen(s);
+if(v%2!=0)
+{
+p=v/2;
+s[b]='*';
+printf("%s",s);
+}
+else
+{
+p=v/2;
+s[p]='*';
+s[p-1]='*';
+printf("%s",s);
+}
+   
