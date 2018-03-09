a22735df654c8fd3019d04c4828d789a5387e81a
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-09 15:44:06 +0530

    ume.c

---
+++ ume.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+char str[20],p;
+int i,v1=0,v2=0,j;
+void main()
+{
+scanf("%s",str);
+for(i=0;str[i]!='+'&&str[i]!='-'&&str[i]!='*'&&str[i]!='/';i++)
+{
+v1=v1*10+(str[i]-48);
+}
+p=str[i++];
+for(j=i;str[j]!='\0';j++)
+v2=v2*10+(str[j]-48);
+switch(k)
+{
+case '+':
+printf("%d",v1+v2);
+break;
+case '-':
+printf("%d",v1-v2);
+break;
+case '*':
+printf("%d",v1*v2);
+break;
+case '/':
+printf("%d",v1/v2);
+break;
+}
+}
