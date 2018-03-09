dc8d53560ed48e39cec052e48aa71d43ee5fffca
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-09 16:27:18 +0530

    veeuppi.c

---
+++ veeuppi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int v,b,i,m,l;
+printf("Enter two Numbers:");
+scanf("%d%d",&v,&p);	
+for(i=1;i<=a&&i<=p;i++)
+{
+if(v%i==0&&p%i==0)
+{
+m=i;
+}
+}
+l=(v*b)/m;
+printf("%d",l);
+}
