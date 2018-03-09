8accdddad3b6ca0b9688360e19514a46e214a66c
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-09 16:01:09 +0530

    upea.c

---
+++ upea.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+char a[1000];
+int v,p,j,count=0;
+scanf("%[^\n]s",a);
+c=strlen(a);
+for(v=0;v<p;v++)
+{
+for(j=i+1;j<=p;j++)
+{
+if(a[v]==a[j])
+count++;
+}
+}
+if(count==0)
+printf("YES");
+else
+printf("NO");
+getch();
+}
