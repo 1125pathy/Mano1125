9e717d6ecdff1a0822f7d0eb83d4b9fecb007079
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-12 16:01:20 +0530

    tgaaehp.c

---
+++ tgaehp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int v,p,a[100],sum=0;
+printf("Enter the Length: ");
+scanf("%d",&p);
+for(v=0;v<n;v++)
+scanf("%d",&a[v]);
+for(v=0;v<p;v++)
+sum=sum+a[v];
+printf("The SUM is: %d",sum);
+return 0;
+}
