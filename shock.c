1e64453bc574657d0092c5311d6bfad19c216982
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-14 15:52:05 +0530

    shock.c

---
+++ shock.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int v,p,count=0;
+printf("\n Enter the number: ");
+scanf("%d",&v);
+for(p=2;p<v;p++)
+{
+if(v%i==0)
+{
+count++;
+}
+}
+if(count==0)
+{
+printf("NO");
+}
+else
+{
+printf("YES");
+}
+}
