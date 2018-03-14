15ba2a2441071ecc711d738f1024428d208d1549
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-14 15:39:58 +0530

    loan.c

---
+++ loan.c
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.ArrayList;
+public class call
+{
+public static void main(String aa[])
+{
+int s,p,l;
+Scanner s=new Scanner(System.in);
+s=s.nextInt();
+p=s.nextInt();
+l=s.nextInt();
+int p=(s*p*l)/100;
+System.out.println(p);
+}
+}
