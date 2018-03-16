71610683c5e6ced6aaaa6fe757785069786a86b2
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-03-16 15:50:14 +0530

    urmh.c

---
+++ urmh.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int v,a[100],p;
+printf("Enter the N: ");
+scanf("%d",&v);
+printf("\nEnter the N numbers: ");
+for(p=0;p<n;p++)
+{
+scanf("%d",&a[i]);
+}
+for(p=0;p<v;p++);
+{
+if(a[p]<a[p+1])
+{
+continue;
+}
+else
+{
+printf("\n Missing number is: %d",p+1);
+break;
+}
+}
+}
